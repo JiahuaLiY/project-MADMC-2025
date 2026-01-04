9c8dd3b1af43a2183f288c72d34a40d475b73809
JiahuaLiY <jiahua.li.y@gmail.com>
2026-01-04 19:34:15 +0100

    test: add new test data

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
-from mokp import dpApproach, miplApproach
-from mokp.dataIO import loadKPData
+from mokp import dpApproach, milpApproach
+from mokp.dataIO import loadKPData, saveData
 
 import numpy as np
+from tqdm import tqdm
 
 df, capacity = loadKPData("./data/2KP200-TA-0.dat")
 
-m, n = 100, 2
+n = 3
+indices = (df.columns)[1 : n + 1]
+owaWeights = list(range(n, 0, -1))
 
-df = df.head(m)
-colstrs = list(df.columns)
+seeds = [192, 302, 337, 875, 177]
+for m in tqdm(range(10, 90, 10)):
+    for seed in tqdm(seeds, leave=False):
+        np.random.seed(seed)
 
-weights = df["w"].to_numpy(dtype=int)
-values = df[colstrs[1 : n + 1]].to_numpy(dtype=int)
-capacity = int(np.floor(weights.sum() / 2))
+        dfPrime = df.sample(m)
+        weights = dfPrime["w"].to_numpy(dtype=int)
+        values = dfPrime[indices].to_numpy(dtype=int)
+        capacity = int(np.floor(weights.sum() / 2))
 
-data1 = dpApproach(values, weights, capacity)
-print(data1)
+        data1 = dpApproach(values, weights, capacity)
+        data2 = milpApproach(values, weights, capacity, owaWeights)
+
+        saveData(f"data/dpData/{n}KP{m}-DP-SEED-{seed}.log", data1)
+        saveData(f"data/milpData/{n}KP{m}-MILP-SEED-{seed}.log", data2)
